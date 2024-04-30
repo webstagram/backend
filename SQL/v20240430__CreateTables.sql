61e243e9b401d448d36589fe6734502dde31ac64
DavidBudnitskyBBD <davidbu@bbd.co.za>
2024-04-30 22:37:58 +0200

    Add character limits

--- SQL/v20240430__CreateTables.sql
+++ SQL/v20240430__CreateTables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE [Users] (
   [UserId] int,
-  [Name] varchar,
+  [Name] varchar(50),
   PRIMARY KEY ([UserId])
 );
 
 CREATE TABLE [Webs] (
   [WebId] int,
-  [Name] varchar,
+  [Name] varchar(25),
   [UserId] int,
   PRIMARY KEY ([WebId])
   FOREIGN KEY ([UserId]) REFERENCES [Users]([UserId])
@@ -14,13 +14,13 @@ CREATE TABLE [Webs] (
 
 CREATE TABLE [Topics] (
   [TopicId] int,
-  [Name] varchar,
+  [Name] varchar(25),
   PRIMARY KEY ([TopicId])
 );
 
 CREATE TABLE [Posts] (
   [PostId] int,
-  [Caption] varchar,
+  [Caption] varchar(255),
   [TopicId] int,
   [TimeCreated] DateTime,
   [WebId] int,
@@ -32,7 +32,7 @@ CREATE TABLE [Posts] (
 CREATE TABLE [Images] (
   [ImageId] int,
   [PostId] int,
-  [Path] varchar,
+  [Path] varchar(255),
   PRIMARY KEY ([ImageId])
   FOREIGN KEY ([PostId]) REFERENCES [Posts]([PostId])
 );
