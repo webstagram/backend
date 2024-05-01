1f20bda09d453853e39b288fb1ecbd92f7b8974e
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-01 09:37:49 +0200

    updated the frontend URL
    will use ENV varaible on server

--- Express/Middleware/corsMiddleware.js
+++ Express/Middleware/corsMiddleware.js
@@ -1,5 +1,6 @@
+const frontend=process.env.FRONT_END_URL || 'http://localhost:5500';
 const corsMiddleware = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow requests from your frontend
+    res.header('Access-Control-Allow-Origin', frontend); // Allow requests from your frontend
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify methods you want to allow
     next(); // Pass control to the next middleware function
