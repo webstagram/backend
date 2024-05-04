b5f44b95ea305657ecac54c21b57afd61950fafe
DavidBudnitskyBBD <davidbu@bbd.co.za>
2024-05-04 11:27:22 +0200

    endpoint works mostly, just need to actually upload now

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,40 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 const protectedRoute = require('./Controllers/HelloWorldController');
 const loginRoute=require("./Controllers/LoginController");
+app.use(bodyParser.json());
+
 const corsMiddleware=require("./Middleware/corsMiddleware")
 // CORS middleware
+
+app.get('/healthcheck', (request, result) => {
+  result.status(200);
+  result.set('Content-Type', 'text/html');
+  result.send("<h1>Health Good</h1>");
+});
+
+
+app.post('/uploadimage', (req, res) => {
+var successfulUpload=true;
+
+  var sendingThisToS3 = {
+    Bucket: 'webstagram-backend-photo-bucket',
+    Key: req.body.fileName,
+    Body: req.body.fileContent
+  }
+  console.log(sendingThisToS3);
+
+  if (successfulUpload){
+    res.status(200).json({ message: `Image uploaded successfully` });
+  } else {
+    res.status(400).json({ message: `Error in uploading image!` });
+  }
+});
+
 
 // Apply CORS middleware globally
 app.use(corsMiddleware);
@@ -19,10 +47,3 @@ app.listen(PORT, (error) => {
     console.log("Error occurred, server can't start", error);
 }
 );
-
-app.get('/healthcheck', (request, result) => {
-  result.status(200);
-  result.set('Content-Type', 'text/html');
-  result.send("<h1>Health Good</h1>");
-});
-
