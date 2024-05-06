115c7b2e01aec09930e81e0b7cc0b06d74ccaaaf
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-05-06 17:14:06 +0200

    Added refresh tokens

--- Express/Controllers/LoginController.js
+++ Express/Controllers/LoginController.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const {authAndReturnJWT}= require('../Services/LoginService');
+const {authAndReturnJWT, refreshJWT}= require('../Services/LoginService');
 
 router.get('/github/callback', async (request, result) => {
     const requestToken = request.query.code;
@@ -12,5 +12,14 @@ router.get('/github/callback', async (request, result) => {
         result.status(400).send(error.message);
     }
 });
+router.get('/refresh',(request, result) => {
+    const refreshToken=request.header("Authorization");
+    try {
+        const userData = refreshJWT(refreshToken);
+         result.json(userData);
+    } catch (error) {
+        result.status(400).send(error.message);
+    }
+})
 
 module.exports = router;
