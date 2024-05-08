1912b1af9529cf27d9991f7303c0a68e4dc0f5c9
David <157478510+DavidBudnitsky-BBD@users.noreply.github.com>
2024-05-08 21:15:59 +0200

    Revert "Added sql to create likes table (#38)"
    This reverts commit 5dbc86c4beda2448eaa623b8970a001a6a762d5a.

--- SQL/V202405082005__CreateLikesTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [Likes] (
-	[LikeId] INT IDENTITY(1,1) PRIMARY KEY,
-	[WebId] INT NOT NULL,
-	[UserId] INT NOT NULL,
-	FOREIGN KEY ([WebId]) REFERENCES Webs([WebId]),
-	FOREIGN KEY ([UserId]) REFERENCES Users([UserId])
-);
